335c58c7d90afdb4e62856598ecd6da919c81b8d
Kareemashraf12 <ka1468@fayoum.edu.eg>
2024-03-17 00:09:29 +0200

    Battle

---
+++ Battle_System1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit player = new Unit(100, 12, 20, "kareem");
+            Unit enemy = new Unit(100, 12, 20, "ruby");
+            Random ra = new Random();
+
+            while (!player.IsDead && !enemy.IsDead)
+            {
+                Console.WriteLine(player.unitname + " " + player.HP + " . " + enemy.unitname + " " + enemy.HP);
+                Console.WriteLine("player turn ! what will yo do ?");
+                string chiose = Console.ReadLine();
+
+                if (chiose == "a")
+                    player.Attack(enemy);
+                else
+                    player.Heal();
+                if (player.IsDead || enemy.IsDead)
+                    break;
+
+                Console.WriteLine(player.unitname + " " + player.HP + " . " + enemy.unitname + " " + enemy.HP);
+                Console.WriteLine("enmy turn!");
+                int rand = ra.Next(0 ,2);
+                if (rand == 0)
+                    enemy.Attack(player);
+                else
+                    enemy.Heal();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Battle_System1/Unit.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_System
+{
+    internal class Unit
+    {
+        private int curuntHP;
+        private int maxHP;
+        private int healpwoer;
+        private int attackpower;
+        private string UnitName;
+        private Random random;
+        public int HP { get { return curuntHP; } }
+        public string unitname { get { return UnitName; } }
+        public bool IsDead { get { return curuntHP <=0  ; } }
+
+
+        public Unit( int maxHP, int healpwoer, int attackpower, string unitName)
+        {
+           
+            this.maxHP = maxHP;
+            this.curuntHP = maxHP;
+            this.healpwoer = healpwoer;
+            this.attackpower = attackpower;
+            this.UnitName = unitName;
+            this.random = new Random();
+           
+        }
+        public void Attack (Unit unittoAttack)
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int randDamage = (int)(attackpower * rng);
+            unittoAttack.takedamge(randDamage);
+            Console.WriteLine(UnitName +" attack " + unittoAttack.UnitName + " and deals " +  randDamage + " damages");
+        }
+        public void Heal ()
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int heal = (int)(rng * healpwoer);
+            curuntHP = heal + curuntHP > maxHP ? maxHP : curuntHP + heal;
+            Console.WriteLine(unitname + " heals " + heal); 
+        }
+        public void takedamge (int damage)
+        {
+            curuntHP -= damage;
+            if (IsDead)
+            {
+                Console.WriteLine(unitname + " has been defeated !!");
+            }
+
+        }
+    }
+}
